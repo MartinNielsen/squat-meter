3b4d112b1ff5cdf04f3c9bd4303414ec3e3fbfab
Martin Nielsen <martin@example.com>
2025-11-22 06:15:51 +0100

    Refactor service worker to simplify caching logic and improve fetch handling

--- sw.js
+++ sw.js
@@ -1,31 +1,9 @@
-const CACHE_NAME = 'squat-meter-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs',
-  'https://cdn.jsdelivr.net/npm/@tensorflow-models/posenet',
-  'https://cdn.jsdelivr.net/npm/chart.js'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  // Bypass the waiting lifecycle stage.
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      return caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+  // Do nothing, just pass through the request.
+  event.respondWith(fetch(event.request));
 });
